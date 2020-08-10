20521755cb1776cd3f183fe7aff7fbabc04368ef
Alif Rayhan <alif_rayhan_27rpl@student.smktelkom-mlg.sch.id>
2020-08-10 09:21:39 +0700

    sapperkanbanmotion

---
+++ src/components/Sections/Navbar.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { fade } from "svelte/transition";
+  export let segment;
+</script>
+
+<style>
+  nav {
+      width: 100%;
+      /* font-size: 2rem; */
+      padding: 1.5rem 0;
+      background-color: #205;
+      color: #fff;
+      text-align: center;
+      box-shadow: 0 1px 5px hsla(303, 91%, 83%, 0.2);
+      position: fixed;
+      top: 0;
+      left: 0;
+      height: 3rem;
+      z-index: 5;
+    }
+  
+    div {
+      height: 6rem;
+    }
+  
+    span {
+      font-size: 1rem;
+      margin-left: 0.2rem;
+    }
+    #right {
+      margin-left: 0rem;
+    }
+</style>
+
+<nav>
+  <h1>
+    <a href="./">
+      {#if segment}
+        KM
+        <span>{segment}</span>
+      {:else}
+        K<span>ANBAN</span>
+        M<span id="right">OTION</span>
+      {/if}
+    </a>
+  </h1>
+</nav>
+<div />
